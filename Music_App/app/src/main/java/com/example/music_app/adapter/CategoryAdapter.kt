db7050d0469778cbc18309898ce81e17f89528bb
BDang029 <dodang159@gmail.com>
2024-05-13 20:03:17 +0700

    Update part 2

--- Music_App/app/src/main/java/com/example/music_app/adapter/CategoryAdapter.kt
+++ Music_App/app/src/main/java/com/example/music_app/adapter/CategoryAdapter.kt
@@ -1,10 +1,16 @@
 package com.example.music_app.adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import com.example.music_app.SongsListActivity
 import com.example.music_app.databinding.CategoryItemRecyclerRowBinding
 import com.example.music_app.models.CategoryModel
+
 
 class CategoryAdapter (private val categoryList : List<CategoryModel>) :
     RecyclerView.Adapter<CategoryAdapter.MyViewHolder>() {
@@ -13,7 +19,17 @@ class CategoryAdapter (private val categoryList : List<CategoryModel>) :
         //bind the data with views
             fun bindData(category:CategoryModel){
                 binding.nameTextView.text = category.name
-
+                Glide.with(binding.coverImageView).load(category.coverUrl)
+                    .apply(
+                        RequestOptions().transform(RoundedCorners(32))
+                    )
+                    .into(binding.coverImageView)
+                //Start SongsList Activity
+                val context = binding.root.context
+                binding.root.setOnClickListener {
+                    SongsListActivity.category = category
+                    context.startActivity(Intent(context,SongsListActivity::class.java))
+                }
 
         }
     }
--- Music_App/app/src/main/java/com/example/music_app/models/CategoryModel.kt
+++ Music_App/app/src/main/java/com/example/music_app/models/CategoryModel.kt
@@ -3,6 +3,7 @@ package com.example.music_app.models
 data class CategoryModel(
     val name:String,
     val coverUrl:String,
+    val songs : List<String>
 ){
-    constructor(): this("","")
+    constructor(): this("","",listOf())
 }
