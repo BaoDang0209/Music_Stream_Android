8b1a0b5333b90c71eebdb950287c833eb074c1ac
zcubffh <125891083+zcubffh@users.noreply.github.com>
2024-06-01 10:18:12 +0700

    Update SignupActivity.kt

--- Music_App/app/src/main/java/com/example/music_app/SignupActivity.kt
+++ Music_App/app/src/main/java/com/example/music_app/SignupActivity.kt
@@ -1,95 +1,81 @@
-package com.example.music_app;
+package com.example.music_app
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputType
 import android.util.Patterns
 import android.view.View
 import android.widget.Toast
-//import com.example.music_app.OTPActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.example.music_app.databinding.ActivitySignupBinding
-import com.google.firebase.FirebaseException
-import com.google.firebase.auth.*
-import java.util.concurrent.TimeUnit
+import com.google.firebase.auth.FirebaseAuth
+import java.util.regex.Pattern
 
 class SignupActivity : AppCompatActivity() {
     lateinit var binding: ActivitySignupBinding
-    private lateinit var auth: FirebaseAuth
-    private lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
-    private var verificationId: String? = null
-    private var isPasswordVisible: Boolean = false
+    private var isPasswordVisible: Boolean = false // Khai báo biến isPasswordVisible
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        auth = FirebaseAuth.getInstance()
-
-        callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
-                // Auto-retrieval or instant validation
-                signInWithPhoneAuthCredential(credential)
-            }
-
-            override fun onVerificationFailed(e: FirebaseException) {
-                setInProgress(false)
-                Toast.makeText(applicationContext, "Xác thực thất bại: ${e.message}", Toast.LENGTH_LONG).show()
-            }
-
-            override fun onCodeSent(verificationId: String, token: PhoneAuthProvider.ForceResendingToken) {
-                setInProgress(false)
-                this@SignupActivity.verificationId = verificationId
-                navigateToOTPActivity(verificationId)
-            }
-        }
-
         binding.createAccountBtn.setOnClickListener {
             val email = binding.emailEdittext.text.toString()
-            val phoneNumber = binding.phoneEdittext.text.toString()
             val password = binding.passwordEdittext.text.toString()
             val confirmPassword = binding.confirmPasswordEdittext.text.toString()
+            val phoneNumber = binding.phoneEdittext.text.toString()
 
+            // Kiểm tra email hợp lệ
+            if (!Pattern.matches(Patterns.EMAIL_ADDRESS.pattern(), email)) {
+                binding.emailEdittext.error = "Invalid email"
+                return@setOnClickListener
+            }
+
+            // Kiểm tra độ dài mật khẩu
             if (password.length < 6) {
-                binding.passwordEdittext.error = "Độ dài phải là 6 ký tự"
-                return@setOnClickListener
-            }
-            if (!password.matches(Regex(".*[A-Z].*"))) {
-                binding.passwordEdittext.error = "Phải chứa ít nhất một chữ cái in hoa"
-                return@setOnClickListener
-            }
-            if (!password.matches(Regex(".*[a-z].*"))) {
-                binding.passwordEdittext.error = "Phải chứa ít nhất một chữ cái thường"
+                binding.passwordEdittext.error = "Length should be 6 char"
                 return@setOnClickListener
             }
 
+            // Kiểm tra mật khẩu có chữ cái viết hoa
+            if (!password.matches(Regex(".*[A-Z].*"))) {
+                binding.passwordEdittext.error = "Must contain at least one uppercase letter"
+                return@setOnClickListener
+            }
+
+            // Kiểm tra mật khẩu có chữ cái viết thường
+            if (!password.matches(Regex(".*[a-z].*"))) {
+                binding.passwordEdittext.error = "Must contain at least one lowercase letter"
+                return@setOnClickListener
+            }
+
+            // Kiểm tra mật khẩu xác nhận có khớp với mật khẩu không
             if (password != confirmPassword) {
                 binding.confirmPasswordEdittext.error = "Mật khẩu không khớp"
                 return@setOnClickListener
             }
 
+            // Kiểm tra email hoặc số điện thoại hợp lệ
             if (email.isNotEmpty()) {
                 if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
                     binding.emailEdittext.error = "Email không hợp lệ"
                     return@setOnClickListener
                 }
-                // Lưu thông tin tài khoản để sau khi OTP hợp lệ mới tạo tài khoản
-                navigateToOTPActivityForEmail(email, password)
             } else if (phoneNumber.isNotEmpty()) {
                 if (!Patterns.PHONE.matcher(phoneNumber).matches()) {
                     binding.phoneEdittext.error = "Số điện thoại không hợp lệ"
                     return@setOnClickListener
                 }
-                setInProgress(true)
-                sendVerificationCode(phoneNumber)
-            } else {
-                Toast.makeText(this, "Vui lòng nhập email hoặc số điện thoại", Toast.LENGTH_SHORT).show()
             }
+
+            createAccountWithFirebase(email, password)
         }
 
         binding.gotoLoginBtn.setOnClickListener {
-            finish()
+            startActivity(Intent(this, LoginActivity::class.java))
         }
+
         binding.passwordToggle.setOnClickListener {
             togglePasswordVisibility()
         }
@@ -97,6 +83,7 @@ class SignupActivity : AppCompatActivity() {
             togglePasswordVisibility()
         }
     }
+
     private fun togglePasswordVisibility() {
         if (isPasswordVisible) {
             binding.confirmPasswordEdittext.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
@@ -117,49 +104,28 @@ class SignupActivity : AppCompatActivity() {
         isPasswordVisible = !isPasswordVisible
     }
 
-    private fun sendVerificationCode(phoneNumber: String) {
-        val options = PhoneAuthOptions.newBuilder(auth)
-            .setPhoneNumber(phoneNumber)
-            .setTimeout(60L, TimeUnit.SECONDS)
-            .setActivity(this)
-            .setCallbacks(callbacks)
-            .build()
-        PhoneAuthProvider.verifyPhoneNumber(options)
-    }
-
-    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
-        auth.signInWithCredential(credential)
-            .addOnCompleteListener(this) { task ->
+    private fun createAccountWithFirebase(email: String, password: String) {
+        setInProgress(true)
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password) // Sửa đổi ở đây
+            .addOnSuccessListener {
                 setInProgress(false)
-                if (task.isSuccessful) {
-                    // Đăng ký thành công
-                    Toast.makeText(applicationContext, "Xác thực thành công", Toast.LENGTH_SHORT).show()
-                    // Chuyển hướng đến màn hình tiếp theo hoặc hoàn thành quá trình đăng ký
-                } else {
-                    // Nếu xác thực thất bại
-                    if (task.exception is FirebaseAuthInvalidCredentialsException) {
-                        Toast.makeText(applicationContext, "Mã OTP không hợp lệ", Toast.LENGTH_SHORT).show()
-                    }
-                }
+                startActivity(Intent(this@SignupActivity, MainActivity::class.java))
+                finish()
+            }.addOnFailureListener {
+                setInProgress(false)
+                Toast.makeText(applicationContext, "Create account failed: ${it.message}", Toast.LENGTH_SHORT).show()
             }
     }
 
-    private fun navigateToOTPActivity(verificationId: String) {
-        val intent = Intent(this, OTPActivity::class.java).apply {
-            putExtra("verificationId", verificationId)
+    override fun onResume() {
+        super.onResume()
+        FirebaseAuth.getInstance().currentUser?.apply {
+            startActivity(Intent(this@SignupActivity, MainActivity::class.java))
+            finish()
         }
-        startActivity(intent)
     }
 
-    private fun navigateToOTPActivityForEmail(email: String, password: String) {
-        val intent = Intent(this, OTPActivity::class.java).apply {
-            putExtra("email", email)
-            putExtra("password", password)
-        }
-        startActivity(intent)
-    }
-
-    private fun setInProgress(inProgress: Boolean) {
+    fun setInProgress(inProgress: Boolean) {
         if (inProgress) {
             binding.createAccountBtn.visibility = View.GONE
             binding.progressBar.visibility = View.VISIBLE
